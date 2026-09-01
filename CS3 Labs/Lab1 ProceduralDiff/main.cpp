e4357cf3c5a92cdd8f189a19197f82cb4d98c3d6
Miles Keffer <mkeffer4@kent.edu>
2026-09-01 15:40:08 -0400

    notes from lab

--- CS3 Labs/Lab1 ProceduralDiff/main.cpp
+++ CS3 Labs/Lab1 ProceduralDiff/main.cpp
@@ -1,1 +1,25 @@
-test
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char ** argv){
+    if (argc < 3) /* or, != to 3*/
+
+}
+
+/*
+-open both files (infile1, infile2)
+-check if files are open, if not, output error
+
+-start reading files via: bool getline(infile1, var)
+    -this will return either a false when it reaches the end of the file
+
+
+
+
+-make check_diff
+    string check_diff(int line_number, string l1, string l2)
+        if l1 and l2
+
+
+*/
